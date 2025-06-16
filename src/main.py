84a7cc128124e14c8ea630677e4a4694b9c6719d
Sebastian Rodriguez <sebastianrodriguezds@gmail.com>
2025-06-16 13:29:49 +0100

    Friend recommendation added feature and test data files

--- src/main.py
+++ src/main.py
@@ -17,8 +17,18 @@ def main():
         
     choice = input("Would you like to display the loaded social network (y/n): ").lower()
     if choice == 'y':
-        print("\nRed socila:")
+        print("\nSocial Network:")
         sn.display_network()
+
+    common_friends = sn.get_common_friends_matrix()
+
+    print("\nFriend recommendations:")
+    for name in sn.members:
+        suggestion = sn.recommend_friend(name, common_friends)
+        if suggestion:
+            print(f"Recommended friend for {name} is {suggestion}")
+        else:
+            print(f"Recommended friend for {name} is none")
 
 if __name__ == "__main__":
     main()
--- src/social_network.py
+++ src/social_network.py
@@ -40,3 +40,41 @@ class SocialNetwork:
     def display_network(self):
         for member in sorted(self.members.values(), key=lambda x: x.name):
             print(member)
+
+    def get_common_friends_matrix(self):
+        member_names = list(self.members.keys())
+        matrix = {}
+
+        for name1 in member_names:
+            row = []
+            friends1 = self.members[name1].friends
+            for name2 in member_names:
+                if name1 == name2:
+                    row.append(0)
+                else:
+                    friends2 = self.members[name2].friends
+                    common = friends1.intersection(friends2)
+                    row.append(len(common))
+            matrix[name1] = row
+        return matrix
+    
+    def recommend_friend(self, member__name, common_friends):
+        if member__name not in self.members:
+            return None
+        
+        member_list = list(self.members.keys())
+        row = common_friends[member__name]
+        max_common = -1
+        recommended  = None
+
+        for i, count in enumerate(row):
+            candidate = member_list[i]
+            if (
+                candidate != member__name and 
+                candidate not in self.members[member__name].friends and
+                count > max_common
+            ):
+                max_common = count
+                recommended = candidate
+
+        return recommended if max_common > 0 else None
